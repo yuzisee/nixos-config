1455525546606fddffb5b20685f750b422a52588
Joseph Huang <yuzisee+github@gmail.com>
2022-08-06 13:21:48 -0700

    Add intel_gpu_top so we can see GPU usage

--- X1Nano1340/etc_nixos/configuration.nix
+++ X1Nano1340/etc_nixos/configuration.nix
@@ -87,6 +87,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    intel-gpu-tools
     vimHugeX
     git
     wlr-randr
