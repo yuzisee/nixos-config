5e80c0f3e761476cf1d15f243bb1992cd0ed616f
Joseph Huang <yuzisee+github@gmail.com>
2022-08-06 12:21:18 -0700

    Okay we figured out how to edit sway config, I think

--- README.md
+++ README.md
@@ -13,3 +13,14 @@ X1Nano1340/joseph
     cd /home/joseph/.config
     ln -s /opt/nixos-config/X1Nano1340/joseph nixpkgs
     file ~/.config/nixpkgs/config.nix
+
+
+# General "Getting started" advice
+
+nmcli radio wifi
+sudo nmcli dev wifi connect 0000000000000000000000000000005A password "firesheep"
+nmcli dev wifi list
+
+sudo nixos-rebuild switch --upgrade
+nix-collect-garbage # -d
+nix-store --optimize
--- X1Nano1340/etc_nixos/etc_conf/write-sway-conf.nix
+++ X1Nano1340/etc_nixos/etc_conf/write-sway-conf.nix
@@ -4,11 +4,16 @@
 # Usage
 #   environment.etc."sway/config".source = lib.mkForce (pkgs.callPackage ./write-sway-conf.nix {pkgs.writeText});
 
-writeText "sway.conf" ''
-  set $mod Mod4
-  # ...
-  exec "${kanshi}/bin/kanshi"
-  # ...
-''
+# writeText "sway.conf" ''
+#   set $mod Mod4
+#   # ...
+#   exec "${kanshi}/bin/kanshi"
+#   # ...
+# ''
+
+# ACCORDING TO...
+# ls /nix/store/*-sway-*/etc/sway
+# INSTRUCTIONS ARE:
+# Copy /etc/sway/config to ~/.config/sway/config and edit it to your liking.
 
 # https://discourse.nixos.org/t/how-to-use-pkg-writetext/19098
