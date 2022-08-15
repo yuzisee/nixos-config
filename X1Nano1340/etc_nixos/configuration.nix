7172745e4fea3bf97138bc24896f2b31f1a549d2
Joseph Huang <yuzisee+github@gmail.com>
2022-08-14 20:09:28 -0700

    Trying some swipe dtection
    https://ld.reddit.com/r/swaywm/comments/em0jnx/can_sway_handle_threefour_finger_touchpad_gestures/

--- X1Nano1340/etc_nixos/configuration.nix
+++ X1Nano1340/etc_nixos/configuration.nix
@@ -100,6 +100,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    libinput-gestures
     intel-gpu-tools
     vimHugeX
     git
