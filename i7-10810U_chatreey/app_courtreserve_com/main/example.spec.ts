7feb3a3e4675b851f681170f1327d23c43c9ce23
yuzisee <yuzisee@users.noreply.github.com>
2026-01-18 13:04:46 -0800

    Something is amiss, but progress is being made

--- i7-10810U_chatreey/app_courtreserve_com/main/example.spec.ts
+++ i7-10810U_chatreey/app_courtreserve_com/main/example.spec.ts
@@ -1,18 +1,67 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, errors } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+async function locator_visible(pw_locator: Locator, timeout_ms: number): Promise<boolean>
+{
+  if ((timeout_ms === undefined) || (timeout_ms == null)) {
+    throw new Error('Please call locator_visible with timeout_ms set to some value');
+  }
+
+  try {
+    await pw_locator.waitFor({state: 'visible', timeout: timeout_ms});
+    return true;
+  } catch (e) {
+    if (e instanceof errors.TimeoutError) {
+      return false;
+    } else {
+      throw e;
+    }
+  }
+}
+
+test('try booking pickleball', async ({ page }) => {
+  var start_url = '';
+  if(process.env.U) {
+    if(process.env.P) {
+      start_url = 'https://app.courtreserve.com/Online/MyProfile/MyClubs/13233';
+    }
+  }
+
+  // await expect(start_url).notToBe('');
+  if (start_url == '') {
+    throw new Error('Please set U and P environment variables, so we have some kind of login credentials');
+  }
+
+  await page.goto(start_url);
+
+  console.log('Hello hello hello');
+  await page.locator('body').ariaSnapshot().then(function(val) { console.log(val); } );
+
+  if (await page.getByRole('text').getByText('log in')) {
+    let username_el: Locator = await page.getByRole('textbox').getByPlaceholder('Enter Your Email', {exact: true});
+    if (locator_visible(username_el, 300)) {
+      let passwd_el: Locator = await page.getByRole('textbox').getByPlaceholder('password');
+      if (locator_visible(passwd_el, 300)) {
+        console.log('Not logged in, need to login');
+        await username_el.ariaSnapshot().then(function(val) { console.log(val); } );
+
+        // Click the get started link.
+        await username_el.fill(process.env.U);
+        await passwd_el.fill(process.env.P);
+	//  page.getByRole('link', { name: 'Get started' }).click();
+      }
+    }
+  }
+
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  // await expect(page).toHaveTitle(/Playwright/);
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+// Try:
+//   U=user@name.com P=passwd npx playwright test
+//   npx playwright test main/example.spec.ts
+//   npx playwright test --headed
+//   npx playwright test --ui
