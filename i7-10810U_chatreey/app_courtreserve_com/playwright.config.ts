83ef1ac92657d73f0af3ff695135e48a0c569173
yuzisee <yuzisee@users.noreply.github.com>
2026-02-17 16:36:17 -0800

    Review response to https://github.com/yuzisee/nixos-config/pull/6#discussion_r2819739971

--- i7-10810U_chatreey/app_courtreserve_com/playwright.config.ts
+++ i7-10810U_chatreey/app_courtreserve_com/playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    /* https://playwright.dev/docs/test-use-options#recording-options */
     screenshot: 'on',
   },
 
