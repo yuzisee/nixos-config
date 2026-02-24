d0baa57161e049886bdae37bf07208fad120badb
yuzisee <yuzisee@users.noreply.github.com>
2026-02-24 01:43:17 -0500

    main/example.spec.ts(1,38): error TS2307: Cannot find module '@playwright/test' or its corresponding type declarations.
    main/example.spec.ts(24,44): error TS2304: Cannot find name 'Locator'.
    main/example.spec.ts(57,37): error TS2304: Cannot find name 'Page'.
    
    * https://www.typescriptlang.org/tsconfig/#moduleResolution
    * https://playwright.dev/docs/test-typescript#manually-compile-tests-with-typescript

--- i7-10810U_chatreey/dev-github/reinstall_playwright.sh
+++ i7-10810U_chatreey/dev-github/reinstall_playwright.sh
@@ -11,7 +11,7 @@ cd "$GIT_ROOT"
 if sudo -n -k echo 'this is most likely a Github-hosted runner, because I am able to sudo without a password, even after -k invalidates any existing credentials (and by setting STDIN to /dev/null it is impossible to enter another password)' < /dev/null; then
   cd i7-10810U_chatreey/app_courtreserve_com
 
-  npm ci # requires that we already `npm init playwright@1.56.0` at some point
+  npm ci # requires that we already `npm install --save-dev @playwright/test@1.56.0` at some point
   npx --no-install playwright install chromium
 else
   exit 69 # EX_UNAVAILABLE
